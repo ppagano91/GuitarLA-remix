3deb6cf3b2eddf64b24532a0020c000b0845e45c
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-21 14:20:58 -0300

    Agrego hojas de estilos externas

--- app/root.jsx
+++ app/root.jsx
@@ -16,14 +16,35 @@ export function links() {
   return [
     {
       rel: "stylesheet",
+      href: "https://necolas.github.io/normalize.css/8.0.1/normalize.css",
+    },
+    {
+      rel: "stylesheet",
       href: styles,
+    },
+    {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
+    },
+    {
+      rel: "preconnect",
+      href: "https://fonts.gstatic.com",
+      crossOrigin: "true",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Outfit:wght@400;700;900&display=swap",
     },
   ];
 }
 /* eslint-enable */
 
 export default function App() {
-  return <Document>Hola</Document>;
+  return (
+    <Document>
+      <h1>Hola Mundo!</h1>
+    </Document>
+  );
 }
 
 function Document({ children }) {
