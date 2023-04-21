4308e24c32e0b990e8e38a4243e4f70606f4477d
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-21 13:40:26 -0300

    Agregando información meta con el componente Meta

--- app/root.jsx
+++ app/root.jsx
@@ -1,27 +1,28 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Meta } from "@remix-run/react";
+
+/* eslint-disable */
+export function meta() {
+  return [
+    {
+      charset: "utf-8",
+      title: "GuitarLA - Remix",
+      viewport: "width=device-width,initial-scale=1",
+    },
+  ];
+}
+/* eslint-enable */
 
 export default function App() {
+  return <Document>Hola</Document>;
+}
+
+function Document({ children }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
-        <Links />
       </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
