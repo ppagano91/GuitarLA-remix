39ad225c17e5c56bf68ecbe14f7bbb3046683d1e
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-25 09:49:57 -0300

    Creando un modelo que se ejecute unicamente en el servidor

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,10 +1,9 @@
+import { getGuitarras } from "~/models/guitarras.server";
 export async function loader() {
-  const respuesta = await fetch(
-    `${process.env.API_URL}/guitarras?populate=imagen`
-  );
-  const resultado = await respuesta.json();
-  console.log(resultado);
-  return [];
+  const guitarras = await getGuitarras();
+  console.log(guitarras);
+
+  return guitarras;
 }
 
 const Tienda = () => {
