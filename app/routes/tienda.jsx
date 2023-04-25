49d08f716db9e6fb40d58a504acbec6c62df131e
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-25 09:45:50 -0300

    Consumiendo el Backend de Strapi con un loader usando fetch

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,3 +1,12 @@
+export async function loader() {
+  const respuesta = await fetch(
+    `${process.env.API_URL}/guitarras?populate=imagen`
+  );
+  const resultado = await respuesta.json();
+  console.log(resultado);
+  return [];
+}
+
 const Tienda = () => {
   return <div>Tienda</div>;
 };
