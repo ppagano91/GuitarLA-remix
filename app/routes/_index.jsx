aba2f60fcf1bf3b4aedc457e0e3a274162d5b8b3
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-27 10:04:52 -0300

    init página Inicio

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,5 +1,40 @@
+import { useLoaderData } from "@remix-run/react";
+import { getGuitarras } from "~/models/guitarras.server";
+import { getPosts } from "~/models/posts.server";
+import ListadoGuitarra from "~/components/listado-guitarras";
+
+import stylesGuitarras from "~/styles/guitarras.css";
+
+export function meta() {
+  return [];
+}
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: stylesGuitarras,
+    },
+  ];
+}
+
+export async function loader() {
+  const [guitarras, posts] = await Promise.all([getGuitarras(), getPosts()]);
+
+  // console.log(guitarras);
+  // console.log(posts);
+  return { guitarras: guitarras.data, posts: posts.data };
+}
+
 const Index = () => {
-  return <div>Inicio</div>;
+  const { guitarras, posts } = useLoaderData();
+  return (
+    <>
+      <main className="contenedor">
+        <ListadoGuitarra guitarras={guitarras} />
+      </main>
+    </>
+  );
 };
 
 export default Index;
