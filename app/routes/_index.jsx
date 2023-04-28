42e55eb8d96d9f5c4ebe412763a36c0bcc440a05
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-27 21:08:44 -0300

    Agrego sección curso a página Inicio + estilos

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,12 +1,16 @@
 import { useLoaderData } from "@remix-run/react";
 import { getGuitarras } from "~/models/guitarras.server";
 import { getPosts } from "~/models/posts.server";
+import { getCurso } from "~/models/curso.server";
 
 import ListadoGuitarra from "~/components/listado-guitarras";
 import stylesGuitarras from "~/styles/guitarras.css";
 
 import ListadoPosts from "../components/listado-posts";
 import stylesPosts from "~/styles/blog.css";
+
+import Curso from "../components/curso";
+import stylesCurso from "~/styles/curso.css";
 
 export function meta() {
   return [];
@@ -22,21 +26,30 @@ export function links() {
       rel: "stylesheet",
       href: stylesPosts,
     },
+    {
+      rel: "stylesheet",
+      href: stylesCurso,
+    },
   ];
 }
 
 export async function loader() {
-  const [guitarras, posts] = await Promise.all([getGuitarras(), getPosts()]);
-  return { guitarras: guitarras.data, posts: posts.data };
+  const [guitarras, posts, curso] = await Promise.all([
+    getGuitarras(),
+    getPosts(),
+    getCurso(),
+  ]);
+  return { guitarras: guitarras.data, posts: posts.data, curso: curso.data };
 }
 
 const Index = () => {
-  const { guitarras, posts } = useLoaderData();
+  const { guitarras, posts, curso } = useLoaderData();
   return (
     <>
       <section className="contenedor">
         <ListadoGuitarra guitarras={guitarras} />
       </section>
+      <Curso curso={curso.attributes} />
       <section className="contenedor">
         <ListadoPosts posts={posts} />
       </section>
