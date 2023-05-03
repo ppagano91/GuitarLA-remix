d46ce55592c0d4898c7d60f9d45ae4907f89abe6
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-05-03 09:37:33 -0300

    Mostrar el contenido del carrito

--- app/root.jsx
+++ app/root.jsx
@@ -77,6 +77,7 @@ export default function App() {
       <Outlet
         context={{
           agregarCarrito,
+          carrito,
         }}
       />
     </Document>
--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useOutletContext } from "@remix-run/react";
+
 import styles from "~/styles/carrito.css";
 
 export function links() {
@@ -19,17 +21,43 @@ export function meta() {
   ];
 }
 const Carrito = () => {
+  const { carrito } = useOutletContext();
   return (
     <main className="contenedor">
       <h1 className="heading">Carrito de Compras</h1>
       <div className="contenido">
         <div className="carrito">
           <h2>Articulos</h2>
+          {carrito.length === 0
+            ? "Carrito vacio"
+            : carrito.map((producto) => (
+                <div key={producto.id} className="producto">
+                  <div>
+                    <img
+                      className="imagen"
+                      src={producto.imagen}
+                      alt={`Imagen del producto ${producto.nombre}`}
+                    />
+                  </div>
+                  <div>
+                    <p className="nombre">{producto.nombre}</p>
+                    <p className="precio">
+                      $<span>{producto.precio}</span>
+                    </p>
+                    <p className="precio">
+                      {" "}
+                      Subtotal: $
+                      <span>{producto.cantidad * producto.precio}</span>
+                    </p>
+                  </div>
+                </div>
+              ))}
+
+          <aside className="resumen">
+            <h3>Resumen del pedido</h3>
+            <p>Total a Pagar: $</p>
+          </aside>
         </div>
-        <aside className="resumen">
-          <h3>Resumen del pedido</h3>
-          <p>Total a Pagar: $</p>
-        </aside>
       </div>
     </main>
   );
