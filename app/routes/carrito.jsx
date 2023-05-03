572b1e251b6cdd25421a595cb5709f9c7dbf7914
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-05-03 12:45:03 -0300

    Se instala remix-utils para corregir problemas de hidratación

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useOutletContext } from "@remix-run/react";
+import { ClientOnly } from "remix-utils";
 
 import styles from "~/styles/carrito.css";
 
@@ -34,68 +35,72 @@ const Carrito = () => {
   }, [carrito]);
 
   return (
-    <main className="contenedor">
-      <h1 className="heading">Carrito de Compras</h1>
-      <div className="contenido">
-        <div className="carrito">
-          <h2>Articulos</h2>
-          {carrito?.length === 0
-            ? "Carrito vacio"
-            : carrito?.map((producto) => (
-                <div key={producto.id} className="producto">
-                  <div>
-                    <img
-                      className="imagen"
-                      src={producto.imagen}
-                      alt={`Imagen del producto ${producto.nombre}`}
-                    />
-                  </div>
-                  <div>
-                    <p className="nombre">{producto.nombre}</p>
-                    <p className="cantidad">Cantidad:</p>
-                    <select
-                      className="select"
-                      name=""
-                      id=""
-                      value={producto.cantidad}
-                      onChange={(e) =>
-                        actualizarCantidad({
-                          cantidad: parseInt(e.target.value),
-                          id: producto.id,
-                        })
-                      }
-                    >
-                      {CANTIDAD_GUITARRAS.map((cantidad) => (
-                        <option value={cantidad} key={cantidad}>
-                          {cantidad}
-                        </option>
-                      ))}
-                    </select>
-                    <p className="precio">
-                      Precio unidad: <span>$ {producto.precio}</span>
-                    </p>
-                    <p className="subtotal">
-                      {" "}
-                      Subtotal:
-                      <span>$ {producto.cantidad * producto.precio}</span>
-                    </p>
-                  </div>
-                  <button
-                    type="button"
-                    className="btn-eliminar"
-                    onClick={() => eliminarGuitarra(producto.id)}
-                  >
-                    X
-                  </button>
-                </div>
-              ))}
-        </div>
-        <aside className="resumen">
-          <h3>Resumen del pedido</h3>
-          <p>Total a Pagar: ${total}</p>
-        </aside>
-      </div>
-    </main>
+    <ClientOnly fallback={"Cargando..."}>
+      {() => (
+        <main className="contenedor">
+          <h1 className="heading">Carrito de Compras</h1>
+          <div className="contenido">
+            <div className="carrito">
+              <h2>Articulos</h2>
+              {carrito?.length === 0
+                ? "Carrito vacio"
+                : carrito?.map((producto) => (
+                    <div key={producto.id} className="producto">
+                      <div>
+                        <img
+                          className="imagen"
+                          src={producto.imagen}
+                          alt={`Imagen del producto ${producto.nombre}`}
+                        />
+                      </div>
+                      <div>
+                        <p className="nombre">{producto.nombre}</p>
+                        <p className="cantidad">Cantidad:</p>
+                        <select
+                          className="select"
+                          name=""
+                          id=""
+                          value={producto.cantidad}
+                          onChange={(e) =>
+                            actualizarCantidad({
+                              cantidad: parseInt(e.target.value),
+                              id: producto.id,
+                            })
+                          }
+                        >
+                          {CANTIDAD_GUITARRAS.map((cantidad) => (
+                            <option value={cantidad} key={cantidad}>
+                              {cantidad}
+                            </option>
+                          ))}
+                        </select>
+                        <p className="precio">
+                          Precio unidad: <span>$ {producto.precio}</span>
+                        </p>
+                        <p className="subtotal">
+                          {" "}
+                          Subtotal:
+                          <span>$ {producto.cantidad * producto.precio}</span>
+                        </p>
+                      </div>
+                      <button
+                        type="button"
+                        className="btn-eliminar"
+                        onClick={() => eliminarGuitarra(producto.id)}
+                      >
+                        X
+                      </button>
+                    </div>
+                  ))}
+            </div>
+            <aside className="resumen">
+              <h3>Resumen del pedido</h3>
+              <p>Total a Pagar: ${total}</p>
+            </aside>
+          </div>
+        </main>
+      )}
+    </ClientOnly>
   );
 };
 
